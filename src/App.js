05000b223d07bbc4df4b42b06136cdd3b2c9c1f3
Mat Jordan <mat@utk.edu>
2021-02-02 23:30:33 -0500

    Mirador says, "Hello React." :wave:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Mirador from './components/Mirador';
+
+// config
+// https://github.com/ProjectMirador/mirador/blob/master/src/config/settings.js
 
 class App extends Component {
   constructor(props) {
@@ -14,6 +18,27 @@ class App extends Component {
     return (
       <div className="container">
         <h1>{title}</h1>
+        <Mirador
+          config={{
+            id: 'mirador',
+            window: {
+              allowFullscreen: false,
+              sideBarPanel: 'info',
+              hideWindowTitle: true,
+              sideBarOpen: true,
+            },
+            windows: [
+              {
+                loadedManifest:
+                  'https://purl.stanford.edu/bk785mr1006/iiif/manifest',
+              },
+            ],
+            workspaceControlPanel: {
+              enabled: false,
+            },
+          }}
+          plugins={[]}
+        />
       </div>
     );
   }
